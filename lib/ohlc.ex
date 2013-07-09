6cb860b3dd6a96c0cb635e86a2c304180591b9a6
Jônatas Davi Paganini <jonatasdp@gmail.com>
2013-07-09 02:02:38 -0300

    refactoring and learning

--- lib/ohlc.ex
+++ lib/ohlc.ex
@@ -5,19 +5,16 @@ defmodule Ohlc do
   defrecord Event, time: nil, price: nil, size: nil
   defrecord Bar, open_time: nil, open: nil, high: nil, low: nil, close: nil, volume: nil do
     def process(Ohlc.Event[] = event) when is_record(event) do
-       bar = new open_time: event.time, 
-       open: event.price,
+      Ohlc.Bar.new open_time: event.time, 
+        open: event.price,
         high: event.price,
         low: event.price,
         close: event.price,
         volume: event.size
     end
     def process(Ohlc.Bar[] = bar, Ohlc.Event[] = event) do
-      bar |> 
-        _high(event.price) |>
-          _low(event.price) |>
-            _close(event.price) |>
-              _sum_volume(event.size)
+      price = event.price
+      bar |> _high(price) |> _low(price) |> _close(price) |> _sum_volume(event.size)
     end
     def _high(Ohlc.Bar[] = bar, price) do
       if price > bar.high, do: bar = bar.high(price) 
@@ -27,7 +24,7 @@ defmodule Ohlc do
       if price < bar.low, do: bar = bar.low(price) 
       bar 
     end
-    def _close(Ohlc.Bar[] = bar, price), do: bar = bar.close(price) 
-    def _sum_volume(Ohlc.Bar[] = bar, trade_size), do: bar = bar.volume(bar.volume + trade_size) 
+    def _close(Ohlc.Bar[] = bar, price), do: bar.close(price) 
+    def _sum_volume(Ohlc.Bar[] = bar, trade_size), do: bar.volume(bar.volume + trade_size) 
   end
 end
